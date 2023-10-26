ac8988ab8af63ffe547eeaf94798840de5f7a8f1
Caleb <kamenjuhkiiru@gmail.com>
2023-10-26 11:32:19 +0300

    Completed assignment

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
+const name = "Kamenju";
 const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const image = "https://media.licdn.com/dms/image/C5603AQG_qLw3-PX9QQ/profile-displayphoto-shrink_800_800/0/1630082390508?e=2147483647&v=beta&t=j0mg16gYGAv7fpFOAYQW3Kdt5dbonuTQoR-BmEUOPE0";
 
 export { name, city, image };
